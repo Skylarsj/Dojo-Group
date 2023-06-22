ce98a76512969ab6a287f8af1de829a322e7a618
Skylarsj <skylarsj@icloud.com>
2023-06-22 12:39:51 -0700

    Fixed screen issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,18 @@ import './App.css'
 import LogIn from './components/LogIn'
 
 
-  function App() {
-    return (
-      <>
+function App() {
+  return (
+    <div className="bg-cover bg-center h-[950px] w-[550px] relative" style={{ backgroundImage: `url('./src/img/original-42f52449520e8e7940c668566888d84f.png')` }}>
+      <div className="absolute w-[61.5%] h-[35.7%] top-[44.2%] left-[52%] transform -translate-x-1/2 -translate-y-1/2">
         <BrowserRouter>
           <Routes>
-            <Route element={<LogIn/>}path="/"/>
+            <Route element={<LogIn />} path="/" />
           </Routes>
         </BrowserRouter>
-      </>
-    );
-  }
+      </div>
+    </div>
+  );
+}
 
 export default App
