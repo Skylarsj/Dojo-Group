f3a2b5211a7bb3caa1b27f720b0b89e474fd81e2
Skylarsj <skylarsj@icloud.com>
2023-06-22 11:52:09 -0700

    Added background img

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
-function App() {
-
-  return (
-    <>
-    <div  className="" >
-        <h1 className="bg-black font-bold text-xl">Hello World</h1>
-      <img className="bg-cover mix-blend-normal" src="./src/img/original-42f52449520e8e7940c668566888d84f.png"/>
-    </div>
-    </>
-  )
-}
+import pokedex from './img/original-42f52449520e8e7940c668566888d84f.png'
+  function App() {
+    return (
+      <>
+        <div className="w-[700px] h-[580px] bg-blend-color-dodge" style={{ backgroundImage: `url(${pokedex})`}}></div>
+      </>
+    );
+  }
 
 export default App
