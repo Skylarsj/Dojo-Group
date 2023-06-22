295f995aa3d60045cd01d1872f06434df6a91dea
Skylarsj <skylarsj@icloud.com>
2023-06-22 13:35:48 -0700

    Login now at bottom of Pokedex

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import LogIn from './components/LogIn'
 function App() {
   return (
     <div className="bg-cover bg-center h-[950px] w-[550px] relative" style={{ backgroundImage: `url('./src/img/original-42f52449520e8e7940c668566888d84f.png')` }}>
-      <div className="absolute w-[61.5%] h-[35.7%] top-[44.2%] left-[52%] transform -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute flex flex-col justify-end w-[61.5%] h-[35.7%] top-[44.2%] left-[52%] transform -translate-x-1/2 -translate-y-1/2">
         <BrowserRouter>
           <Routes>
             <Route element={<LogIn />} path="/"/>
--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const LogIn = () => {
     return (
-        <div className="">
-            <div className="relative bg-[#626466] p-10 pb-8 h-70">
-                <form className="txt-center pb-1">
-                    <div className="flex items-center justify-center">
+        <div>
+            <div className="bg-[#626466]">
+                <form className="flex pb-1">
+                    <div>
                         <input
-                            className="mb-5 border rounded-md h-10 border-gray-500 bg-black text-lg pl-1"
-                            placeholder="e-mail"
+                            className="border rounded-md h-10 w-[149px] border-gray-500 bg-[#00C247] font-mono placeholder-black text-lg pl-1"
+                            placeholder="username"
                             type="text"
-                            id="email"
-                            name="email"
+                            id="username"
+                            name="username"
                             value=""
                             onChange=""
                         />
                     </div>
                     <div>
                         <input
-                            className="mb-8 border rounded-md border-gray-500 h-10 bg-black text-lg pl-1"
+                            className="border rounded-md border-gray-500 h-10 w-[150px] bg-[#00C247] font-mono placeholder-black text-lg pl-1"
                             placeholder="password"
                             type="password"
                             id="password"
@@ -27,10 +27,8 @@ const LogIn = () => {
                             onChange=""
                         />
                     </div>
+                        <button className="flex items-center border rounded-md border-gray-500 h-10 w-10 bg-[#00C247] font-mono placeholder-black text-lg pl-2">Go</button>
                 </form>
-                <a
-                href="#" //this will be the link to the sign up page
-                className="underline text-[9px] text-gray-500">Don't have an account?</a>
             </div>
         </div>
     )
