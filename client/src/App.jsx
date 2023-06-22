b3887a12b521149b1dfda5e80ddfbfc24f7ca27c
Skylarsj <skylarsj@icloud.com>
2023-06-22 11:32:46 -0700

    Fixed Tailwind issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-  <img src="./Group Project/Dojo-Group/client/src/img/original-42f52449520e8e7940c668566888d84f.png"/>
+    <div  className="" >
+        <h1 className="bg-black font-bold text-xl">Hello World</h1>
+      <img className="bg-cover mix-blend-normal" src="./src/img/original-42f52449520e8e7940c668566888d84f.png"/>
+    </div>
     </>
   )
 }
