71c2a1b13d1f5d0d26da64a6a32d97493779e914
Skylarsj <skylarsj@icloud.com>
2023-06-25 19:12:08 -0700

    Update

--- client/src/components/NavMap.jsx
+++ client/src/components/NavMap.jsx
@@ -6,6 +6,10 @@ const NavMap = () => {
             <img className="h-16 w-16" src="./src/img/PokemonButton.png" alt="logo"/>
             <p className="font-mono text-black text-[20px] ml-[-10px] pt-6">x</p>
             <p className="font-mono text-black text-[20px] pt-6">25</p>
+            <img className="h-20 w-auto pb-6" src="./src/img/NormalPokeball.png" alt="logo"/>
+            <p className="font-mono text-black text-[20px] ml-[-10px] pt-6">x</p>
+            <p className="font-mono text-black text-[20px] pt-6">1</p>
+            <button className="w-16 border border-black h-auto m-4 ml-auto p-1 text-xs font-mono text-black bg-[#00C247]">Logout</button>
         </div>
     );
 }
