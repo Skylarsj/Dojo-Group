4874123dfdfe3f2e61a26a525e95c7552786495f
Skylarsj <skylarsj@icloud.com>
2023-06-25 19:00:21 -0700

    Adding Navbar logic

--- client/src/components/NavMap.jsx
+++ client/src/components/NavMap.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const NavMap = () => {
     return(
         <div className="flex h-16 w-full bg-[#00C247] border-t-2 border-black">
-            <img className="h-16 w-16" src="https://i.imgur.com/2Z3Z5Xo.png" alt="logo"/>
+            <img className="h-16 w-16" src="./src/img/PokemonButton.png" alt="logo"/>
+            <p className="font-mono text-black text-[20px] ml-[-10px] pt-6">x</p>
+            <p className="font-mono text-black text-[20px] pt-6">25</p>
         </div>
     );
 }
--- client/src/views/navbar.jsx
+++ client/src/views/navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import NavMap from '../components/NavMap';
 const Navbar = () => {
     return(
         <div className="flex h-16 w-full bg-[#00C247] border-t-2 border-black">
+            <NavMap/>
         </div>
     );
 }
