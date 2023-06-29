b45ed48967506526aba86d0026d692e661a5d5cb
JEspi02m <j.espi02@gmail.com>
2023-06-29 06:58:48 -0500

    Updated Server folder structure update init.py , launch.py , and userroutes.py added pokemon schema sql text file

--- server/controllers/pokemonController.py
+++ server/controllers/pokemonController.py
@@ -4,19 +4,19 @@ from server.models.pokemonModel import Pokemon
 from server.models.userModel import User
 
 def all_pokemon():
-    # make a function that will return a json error if the user is not logged in
-    if 'user_id' not in session:
-        return {'error': True, 'message': 'You must be logged in to view this page'}, False
-    user = User.get_id({'id': session['user_id']})
-    if not user:
-        return {'error': True, 'message': 'Failed to get user'}, False
-    pokemon = Pokemon.get_all()
-    if not pokemon:
-        return {'error': True, 'message': 'Failed to get pokemon'}, False
-    return {'error': False, user: user, pokemon: Pokemon.get_all()}
+  # make a function that will return a json error if the user is not logged in
+  if 'user_id' not in session:
+    return {'error': True, 'message': 'You must be logged in to view this page'}, False
+  user = User.get_id({'id': session['user_id']})
+  if not user:
+    return {'error': True, 'message': 'Failed to get user'}, False
+  pokemon = Pokemon.get_all()
+  if not pokemon:
+    return {'error': True, 'message': 'Failed to get pokemon'}, False
+  return {'error': False, user: user, pokemon: Pokemon.get_all()}
 
 def get_pokemon_info(pokemon_name):
-    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"   
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
@@ -56,3 +56,29 @@ def capture_pokemon(pokemon_name, pokeball_type):
         print(f"{pokemon_name} captured successfully using {pokeball_type}!")
     else:
         print(f"{pokemon_name} escaped. Try again!")
+
+def generate_encounter(habitat):
+    # Define a list of valid habitats
+    valid_habitats = ['cave', 'forest', 'grassland', 'mountain', 'rare', 'rough-terrain', 'sea', 'urban', 'waters-edge']
+
+    # Retrieve the list of Pokémon that can be found in the specified habitat
+    url = f"https://pokeapi.co/api/v2/pokemon-habitat/{habitat}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        return {'error': True, 'message': f'Failed to retrieve Pokémon data for {habitat}'}
+
+    data = response.json()
+    pokemon_list = data['pokemon_species']
+
+    # Check if the habitat contains any Pokémon
+    if not pokemon_list:
+        return {'error': True, 'message': f'No Pokémon found for {habitat}'}
+
+    # Select a random Pokémon from the list
+    pokemon_name = random.choice(pokemon_list)['name']
+    
+    # Retrieve information about the selected Pokémon
+    pokemon_info = get_pokemon_info(pokemon_name)
+
+    # Return the result
+    return {'error': False, 'pokemon': pokemon_info}
