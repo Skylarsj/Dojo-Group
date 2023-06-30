87e1e14e2705f8b18ba3141d6433fedb867b0db7
Skylarsj <skylarsj@icloud.com>
2023-06-29 18:16:27 -0700

    Update

--- back-end/server.py
+++ back-end/server.py
@@ -1,5 +1,5 @@
 from flask import Flask
 from server import app
-from server.controllers import userController, pokemonController
+from server.routes import userRoutes, pokemonRoutes
 if __name__ == "__main__":
     app.run(debug=True)
